931764a22ffb4d0293070b7423d751da710c925d
quanvuong <quan.vuong@nyu.edu>
2016-05-06 20:55:22 +0400

    Cleaned up unnecessary

--- src/client.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { Router } from 'react-router'
-import routes from "./lib/routes"
-import ReactDOM from "react-dom"
-
-ReactDOM.render(
-  <Router routes={routes} />,
-  document.getElementById("main"))
