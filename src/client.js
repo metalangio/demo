b9f8c9bde0d18db49c11d36c6fd812362e1899ce
quanvuong <quan.vuong@nyu.edu>
2016-05-06 21:31:52 +0400

    hash history

--- src/client.js
+++ src/client.js
@@ -1,9 +1,8 @@
 import React from "react"
 import { Router } from 'react-router'
-import createBrowserHistory from 'history/lib/createBrowserHistory'
 import routes from "./lib/routes"
 import ReactDOM from "react-dom"
 
 ReactDOM.render(
-  <Router history={createBrowserHistory()} routes={routes} />,
+  <Router routes={routes} />,
   document.getElementById("main"))
