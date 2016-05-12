f0c95f77f211894302639db58a4bf1d18b818ccb
quanvuong <quan.vuong@nyu.edu>
2016-05-12 23:30:41 +0400

    remove hard code value for time of video

--- src/components/VideoApp.js
+++ src/components/VideoApp.js
@@ -10,7 +10,7 @@ export default class VideoApp extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      listOfAnswers: [100, 200, 500]
+      listOfAnswers: []
     }
   }
 
